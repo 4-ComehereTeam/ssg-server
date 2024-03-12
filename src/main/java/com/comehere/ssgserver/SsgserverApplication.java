3a00794a1e00b2ca88b3923218d848f529417f3d
Dahye73 <sindahye730@gmail.com>
2024-03-12 16:14:03 +0900

    init create project

---
+++ src/main/java/com/comehere/ssgserver/SsgserverApplication.java
@@ -0,0 +1,13 @@
+package com.comehere.ssgserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsgserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsgserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comehere/ssgserver/SsgserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comehere.ssgserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsgserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
