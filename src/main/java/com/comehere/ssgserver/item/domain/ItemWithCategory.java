2dcb970baaed4741abb813f94800670fcd2d28a1
pockyjx <0127828b@gmail.com>
2024-03-15 09:47:24 +0900

    fix: item, itemWithCategory 테이블 생성되지 않는 현상 수정

--- src/main/java/com/comehere/ssgserver/item/domain/ItemWithCategory.java
+++ src/main/java/com/comehere/ssgserver/item/domain/ItemWithCategory.java
@@ -2,6 +2,7 @@ package com.comehere.ssgserver.item.domain;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
 
@@ -9,7 +10,7 @@ import lombok.Getter;
 @Getter
 public class ItemWithCategory {
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	private Integer bigCategory;
