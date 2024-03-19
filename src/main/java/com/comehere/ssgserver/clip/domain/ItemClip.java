9e72c2868cd71c6800db65719638676bf715fda1
ddohyeong <dohyeong7026@gmail.com>
2024-03-19 10:33:19 +0900

    fix : ItemClip.java 의 id 전략 수정

--- src/main/java/com/comehere/ssgserver/clip/domain/ItemClip.java
+++ src/main/java/com/comehere/ssgserver/clip/domain/ItemClip.java
@@ -6,6 +6,7 @@ import com.comehere.ssgserver.member.domain.Member;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -13,7 +14,7 @@ import jakarta.persistence.ManyToOne;
 @Entity
 public class ItemClip {
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@ManyToOne(fetch = FetchType.LAZY)
