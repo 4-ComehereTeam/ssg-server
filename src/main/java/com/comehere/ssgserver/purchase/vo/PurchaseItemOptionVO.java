39d79bbb893cc9505019a7c9fe3ed74b357b90ac
ddohyeong <dohyeong7026@gmail.com>
2024-03-26 10:39:12 +0900

    feat: 회원 주문 API 개발

---
+++ src/main/java/com/comehere/ssgserver/purchase/vo/PurchaseItemOptionVO.java
@@ -0,0 +1,14 @@
+package com.comehere.ssgserver.purchase.vo;
+
+import lombok.Getter;
+
+@Getter
+public class PurchaseItemOptionVO {
+	private Long itemOptionId;
+
+	private String itemName;
+
+	private Long itemPrice;
+
+	private Integer count;
+}
