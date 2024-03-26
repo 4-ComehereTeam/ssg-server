71cbaa631b5143bc33b063685c7c66bdf383bc5c
pockyjx <0127828b@gmail.com>
2024-03-26 10:55:24 +0900

    feat: 묶음 상품 목록 조회 API 개발

--- src/main/java/com/comehere/ssgserver/bundle/infrastructure/BundleRepository.java
+++ src/main/java/com/comehere/ssgserver/bundle/infrastructure/BundleRepository.java
@@ -1,8 +1,16 @@
 package com.comehere.ssgserver.bundle.infrastructure;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.comehere.ssgserver.bundle.domain.Bundle;
 
 public interface BundleRepository extends JpaRepository<Bundle, Long> {
+	@Query("select b from Bundle b where b.status = true")
+	Page<Bundle> getBundleList(Pageable page);
 }
