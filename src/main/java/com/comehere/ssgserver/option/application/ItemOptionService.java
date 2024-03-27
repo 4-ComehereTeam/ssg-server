65b0a48417522e8445023ec45c7627fc2025f556
pockyjx <0127828b@gmail.com>
2024-03-27 11:20:00 +0900

    feat: 상품 별 옵션 여부 조회 API 개발

--- src/main/java/com/comehere/ssgserver/option/application/ItemOptionService.java
+++ src/main/java/com/comehere/ssgserver/option/application/ItemOptionService.java
@@ -1,7 +1,10 @@
 package com.comehere.ssgserver.option.application;
 
 import com.comehere.ssgserver.option.dto.ItemOptionRespDTO;
+import com.comehere.ssgserver.option.dto.OptionRespDTO;
 
 public interface ItemOptionService {
 	ItemOptionRespDTO findByItemId(Long itemId);
+
+	OptionRespDTO hasOptions(Long itemId);
 }
