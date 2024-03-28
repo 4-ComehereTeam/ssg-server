3e297bda9600a936d159a40982a12454f0005ac7
pockyjx <0127828b@gmail.com>
2024-03-28 09:24:35 +0900

    feat: 옵션 (기타) 조회 API 개발

--- src/main/java/com/comehere/ssgserver/option/domain/Size.java
+++ src/main/java/com/comehere/ssgserver/option/domain/Size.java
@@ -1,5 +1,6 @@
 package com.comehere.ssgserver.option.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -8,10 +9,11 @@ import lombok.Getter;
 
 @Entity
 @Getter
-public class Size {
+public class ItemSize {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@Column(nullable = false, length = 50)
 	private String value;
 }
