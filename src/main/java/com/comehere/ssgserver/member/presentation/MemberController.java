165b54ef11fe48b66b510b76f67beecedaffb4fe
Dahye73 <sindahye730@gmail.com>
2024-03-30 22:53:49 +0900

    fix : 충돌 파일 수정

--- src/main/java/com/comehere/ssgserver/member/presentation/MemberController.java
+++ src/main/java/com/comehere/ssgserver/member/presentation/MemberController.java
@@ -5,7 +5,7 @@ import java.util.UUID;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,7 +41,7 @@ public class MemberController {
 		return "Main Controller";
 	}
 
-	@PostMapping("/modify/password")
+	@PutMapping("/modify/password")
 	@Operation(summary = "비밀번호 변경")
 	public BaseResponse<?> userModifyPassword(@RequestHeader("Authorization") String accessToken,
 			@RequestBody ModifyPwdRequestVO modifyPwdRequestVo) {
