a8fb4e33723dc9658b761841e7547dd05c873f34
Dahye73 <sindahye730@gmail.com>
2024-04-02 21:41:55 +0900

    feat : 장바구니에 상품 담기 기능 개발

---
+++ src/main/java/com/comehere/ssgserver/cart/application/CartServiceImpl.java
@@ -0,0 +1,39 @@
+package com.comehere.ssgserver.cart.application;
+
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import com.comehere.ssgserver.cart.domain.Cart;
+import com.comehere.ssgserver.cart.dto.request.AddProductReqDTO;
+import com.comehere.ssgserver.cart.infrastructure.CartRepository;
+import com.comehere.ssgserver.purchase.infrastructure.AddressRepository;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class CartServiceImpl implements CartService {
+
+	private final CartRepository cartRepository;
+	private final AddressRepository addressRepository;
+
+	@Override
+	public Boolean addProductToCart(UUID uuid, AddProductReqDTO addProductReqDTO) {
+
+		cartRepository.save(addProduct(uuid, addProductReqDTO));
+		return true;
+	}
+
+	private Cart addProduct(UUID uuid, AddProductReqDTO addProductReqDTO) {
+
+		return Cart.builder()
+				.uuid(uuid)
+				.itemOptionId(addProductReqDTO.getItemOptionId())
+				.itemCount(addProductReqDTO.getItemCount())
+				.itemCheck(false)
+				.memberAddressId(addressRepository.getDefaultAddress(uuid))
+				.pinStatus(false)
+				.build();
+	}
+}
---
+++ src/main/java/com/comehere/ssgserver/cart/vo/request/AddProductReqVO.java
@@ -0,0 +1,10 @@
+package com.comehere.ssgserver.cart.vo.request;
+
+import lombok.Getter;
+
+@Getter
+public class AddProductReqVO {
+
+	private Long itemOptionId;
+	private Integer itemCount;
+}
