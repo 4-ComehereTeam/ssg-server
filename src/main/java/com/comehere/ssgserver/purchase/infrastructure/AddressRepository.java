5c9561e2693ac11e56df996a0cce8578ce193939
Dahye73 <sindahye730@gmail.com>
2024-04-08 11:20:31 +0900

    feat: 기본 배송지 변경 기능 개발

--- src/main/java/com/comehere/ssgserver/purchase/infrastructure/AddressRepository.java
+++ src/main/java/com/comehere/ssgserver/purchase/infrastructure/AddressRepository.java
@@ -1,7 +1,6 @@
 package com.comehere.ssgserver.purchase.infrastructure;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,6 +9,4 @@ import com.comehere.ssgserver.purchase.domain.Address;
 
 public interface AddressRepository extends JpaRepository<Address, Long>, CustomAddressRepository {
 	List<Address> findAllByUuid(UUID uuid);
-
-	Optional<Address> findByIdAndUuid(Long id, UUID uuid);
 }
--- src/main/java/com/comehere/ssgserver/purchase/presentation/AddressController.java
+++ src/main/java/com/comehere/ssgserver/purchase/presentation/AddressController.java
@@ -3,6 +3,7 @@ package com.comehere.ssgserver.purchase.presentation;
 import org.modelmapper.ModelMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +13,9 @@ import com.comehere.ssgserver.common.response.BaseResponse;
 import com.comehere.ssgserver.common.security.jwt.JWTUtil;
 import com.comehere.ssgserver.purchase.application.AddressService;
 import com.comehere.ssgserver.purchase.dto.req.AddressAddReqDTO;
+import com.comehere.ssgserver.purchase.dto.req.DefaultChangeReqDTO;
 import com.comehere.ssgserver.purchase.vo.req.AddressAddReqVO;
+import com.comehere.ssgserver.purchase.vo.req.DefaultChangeReqVO;
 import com.comehere.ssgserver.purchase.vo.resp.AddressListRespVO;
 import com.comehere.ssgserver.purchase.vo.resp.DefaultCheckRespVO;
 
@@ -27,7 +30,7 @@ import lombok.AllArgsConstructor;
 public class AddressController {
 
 	private final JWTUtil jwtUtil;
-	private final AddressService addressService; // 'AddressService' has no 'getDefaultAddress' method
+	private final AddressService addressService;
 	private final ModelMapper modelMapper;
 
 	@GetMapping("/default")
@@ -38,6 +41,15 @@ public class AddressController {
 		return new BaseResponse<>(
 				modelMapper.map(addressService.getDefaultAddress(jwtUtil.getUuidByAuthorization(accessToken)),
 						DefaultCheckRespVO.class));
+	}
+
+	@PutMapping("/default/change")
+	@Operation(summary = "기본 배송지 변경")
+	public BaseResponse<Boolean> userDefaultAddressChange(@RequestHeader("Authorization") String accessToken,
+			@RequestBody DefaultChangeReqVO defaultChangeReqVO) {
+
+		return new BaseResponse<>(addressService.changeDefaultAddress(jwtUtil.getUuidByAuthorization(accessToken),
+				modelMapper.map(defaultChangeReqVO, DefaultChangeReqDTO.class)));
 	}
 
 	@PostMapping("/")
@@ -53,7 +65,7 @@ public class AddressController {
 	@GetMapping("/list")
 	@Operation(summary = "배송지 조회")
 	public BaseResponse<AddressListRespVO> userAddressDetail(@RequestHeader("Authorization") String accessToken) {
-		
+
 		return new BaseResponse<>(modelMapper.map(addressService
 				.getAddressList(jwtUtil.getUuidByAuthorization(accessToken)), AddressListRespVO.class));
 	}
