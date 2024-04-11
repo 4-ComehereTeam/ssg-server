9297ef91ceace0e8a7739fc79d4389d538688dde
pockyjx <0127828b@gmail.com>
2024-04-11 09:20:14 +0900

    feat: 특가 상품 스케줄러 적용

--- src/main/java/com/comehere/ssgserver/bundle/infrastructure/BundleRepository.java
+++ src/main/java/com/comehere/ssgserver/bundle/infrastructure/BundleRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import com.comehere.ssgserver.bundle.domain.Bundle;
@@ -13,4 +14,8 @@ import com.comehere.ssgserver.bundle.domain.Bundle;
 public interface BundleRepository extends JpaRepository<Bundle, Long>, CustomBundleRepository {
 	@Query("select b from Bundle b where b.status = true")
 	Page<Bundle> getBundleList(Pageable page);
+
+	@Modifying
+	@Query("update Bundle b set b.status = false where b.finishDate < current_date")
+	void updateBundleStatus();
 }
